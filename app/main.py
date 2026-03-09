d8878c9b204a399091a3cedcedc4b89b7ab75d35
Dmytro <dima.shutko5@gmail.com>
2026-03-09 01:55:13 +0100

    solution2

--- app/main.py
+++ app/main.py
@@ -20,19 +20,26 @@ def move_file(command: str) -> None:
 
         if "/" in destination_file_name:
 
-            *path_folders, _ = destination_file_name.split("/")
+            if destination_file_name[-1] == "/":
+                *path_folders, = destination_file_name.split("/")
+            else:
+                *path_folders, = destination_file_name.split("/")[:-1]
 
             for i in range(len(path_folders)):
-                path_to_folder = "/".join(path_folders[: i + 1])
+                path_to_folder = os.path.join(*path_folders[: i + 1])
 
                 try:
                     os.mkdir(path_to_folder)
 
                 except FileExistsError:
-                    print(f"folder {path_to_folder} exists!")
+                    pass
+
+        if destination_file_name[-1] == "/":
+            destination_file_name = os.path.join(destination_file_name,
+                                                 source_file_name)
 
         with open(destination_file_name, "w") as destination_file:
             destination_file.write(content)
 
     except FileNotFoundError:
-        print(f'file "{source_file_name}" not found')
+        pass
